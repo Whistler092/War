b37eeaf46b69237a6b290c4d57629c4064fa3b9f
Ramiro Andrés Bedoya <iamramiroo@gmail.com>
2014-12-24 14:11:29 -0500

    Agregadas imágenes y presentadas de forma ramdon
    Agregadas las imágenes y presentadas de forma aleatoria

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -26,6 +26,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func playRoundTapped(sender: UIButton) {
+        //self.playRoundButton.setTitle("Play Round", forState: UIControlState.Normal)
+        
+        //Randomize a number for the fist imageView
+        var fistRandomNumber = arc4random_uniform(13) + 1
+        var secondRandomNumber = arc4random_uniform(13) + 1
+        
+        //Constructor a String whit the randomNumber
+        var fistCardString:String = String(format: "%i", fistRandomNumber)
+        fistCardString += GetRandomText()
+        
+        var secondCardString:String = String(format: "%i", secondRandomNumber )
+        secondCardString += GetRandomText()
+        
+        
+        self.fistCardImageView.image = UIImage(named: fistCardString)
+        self.secondCardImageView.image = UIImage(named: secondCardString)
+        
+    }
+    
+    func GetRandomText() -> String{
+        var fistRandomCardType =  arc4random_uniform(4)+1
+        var textCard = ""
+        if fistRandomCardType == 1{
+            textCard = "_of_clubs"
+        }
+        else if fistRandomCardType == 2 {
+            textCard = "_of_diamonds"
+        }
+        else if fistRandomCardType == 3 {
+            textCard = "_of_hearts"
+        }
+        else{
+            textCard = "_of_spades"
+        }
+        
+        return textCard
+    }
 
 }
 
